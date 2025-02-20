c3c0d5c1adfbc5d0c45177483df2aa15577adb58
mazpen <Azpensbri@gmail.com>
2025-02-20 11:22:37 -0500

    s7c2: optimization footer

--- footer.php
+++ footer.php
@@ -8,19 +8,22 @@
                 )); ?>
             </div>
             <div class="piedpage__s1__adresse">
-                <div class="piedpage__s1__adresse__coord">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere porro veniam vitae, tempore corporis omnis nam 
-                </div>
-                <div class="piedpage__s1__adresse__recherche">
-                    <?php get_search_form();   ?>
-                </div>
+                
             </div>
-            <div class="piedpage__s1__description">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat vero explicabo iure sit enim, ea ducimus nesciunt inventore impedit blanditiis unde omnis facere, deleniti eligendi fuga molestias dolor eveniet laborum!
-            </div>
+            
         </section>
         <section class="piedpage__s2"></section>
+            <div class="piedpage__s2__adresse__coord">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere porro veniam vitae, tempore corporis omnis nam 
+            </div>
+            <div class="piedpage__s2__adresse__recherche">
+                <?php get_search_form();   ?>
+            </div>
+
         <section class="piedpage__s3"></section>
+        <div class="piedpage__s3__description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat vero explicabo iure sit enim, ea ducimus nesciunt inventore impedit blanditiis unde omnis facere, deleniti eligendi fuga molestias dolor eveniet laborum!
+            </div>
 
 
     </div>
