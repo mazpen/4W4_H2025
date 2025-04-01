ec9a7c650c1bde2fd735fea664f17c2b0a946c55
mazpen <Azpensbri@gmail.com>
2025-04-01 10:57:24 -0400

    s10c1-exam intra: ajout de 404.scss

--- 404.php
+++ 404.php
@@ -1,13 +1,19 @@
-<?php get_header() ?>
-<h1>404</h1>
-    <section class="404">
-        <div class="404_global">
-            <article class="404__article">
-                <p>Oops, vous avez échoué sur l'île 404 !</p>
-                <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d'exception !</p>
-            </article>
-        </div>
-    </section>
-    <?php get_footer(); ?>
-</body>
-</html>
+<?php get_header(); ?>
+
+  <p class="erreur_404_texte">
+    <br>
+    <strong>Oups !</strong> Il semblerait que vous ayez atterri sur une page qui n'existe pas.
+  Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !
+  </p>
+  <p class="bouton_acceuil">
+    <!-- Bouton accueil -->
+    <a href="<?php echo home_url(); ?>">Revenir à l'accueil</a> 
+  </p>
+  <?php wp_nav_menu(array(
+                    'menu' => 'erreur_404',
+                    'container' => 'nav',
+                    'container_class' => 'erreur_404_menu'
+  )); ?>
+</section>
+
+<?php get_footer(); ?>
