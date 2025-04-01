6ba0a68167e42c3d61a67810fc294bd48d4662da
mazpen <Azpensbri@gmail.com>
2025-04-01 10:31:43 -0400

    s10c1-exam intra: ajout 404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+<h1>404</h1>
+    <section class="404">
+        <div class="404_global">
+            <article class="404__article">
+                <p>Oops, vous avez échoué sur l'île 404 !</p>
+                <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d'exception !</p>
+            </article>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+</body>
+</html>
