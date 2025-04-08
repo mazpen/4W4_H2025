4084073890a55050549b9895880d88a2aa33f77d
mazpen <Azpensbri@gmail.com>
2025-04-08 10:02:14 -0400

    s11c1:optimization JS + bouton-categorie scss

--- index.php
+++ index.php
@@ -9,10 +9,7 @@
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article class="populaire__article">
-                <?php 
-                    if (has_post_thumbnail()) {
-                    the_post_thumbnail('thumbnail'); }
-                ?>
+             
                 <h2 class="populaire__titre"><?php the_title(); ?></h2>
                 <div class="populaire__contenu"><?php the_content(); ?></div>
             </article>
--- js/destination.js
+++ js/destination.js
@@ -1,38 +1,39 @@
 (function(){
-    console.log("vive javascript")
+    console.log("vive javascript");
 
-
-        const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+        let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
         const domaine = window.location.href;
-        let apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
         //const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+        let apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
         const categorie__ul__li = document.querySelectorAll(".categorie__ul__li");
-        console.log("categorie__ul__li",categorie__ul__li);
+        console.log("categorie__ul__li.length",categorie__ul__li.length);
+        mon_fetch(apiUrl);
         categorie__ul__li.forEach(li => {
-
-            li.addEventListener('click', function(){
-            console.log(li.dataset.id);
-            categoryId = li.dataset.id;
-            apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${li.dataset.id}`;
+            li.addEventListener("mousedown", function(){
+                console.log(li.dataset.id);
+                categoryId = li.dataset.id;
+                apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
+                console.log("apiUrl = ",apiUrl);
+                mon_fetch(apiUrl);
             })
         })
-    
-        fetch(apiUrl)
+        function mon_fetch(apiUrl){
+            fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
                 const destinationList = document.querySelector('.destination__list');
+                destinationList.innerHTML = "";
                 data.forEach(article => {
                     const articleElement = document.createElement('div');
                     articleElement.innerHTML = `
                         <h3>${article.title.rendered}</h3>
-                        <div>${article.excerpt.rendered}</div>
                         <a href="${article.link}">Lire plus</a>
                     `;
-                    destinationList .appendChild(articleElement);
+                    destinationList.appendChild(articleElement);
                 });/**
                 *  Script js permettant d'extraite des destinations de voyage
                 */
-               (function(){
+            (function(){
                    console.log("destination.js")
                    const categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
                    const domaine = window.location.href
@@ -46,26 +47,27 @@
                }
                
                
-                   fetch(apiUrl)
-                       .then(response => response.json())
-                       .then(data => {
-                           const destinationList = document.querySelector('.destination__list');
-                           data.forEach(article => {
-                               const articleElement = document.createElement('div');
-                               console.log(article.title.rendered)
-                               // <div>${article.excerpt.rendered}</div>
-                               articleElement.innerHTML = `
-                                   <h3>${article.title.rendered}</h3>
-               
-                                   <a href="${article.link}">Lire plus</a>
-                               `;
-                               destinationList .appendChild(articleElement);
-                           });
-                       })
-                       .catch(error => console.error('Erreur lors de la récupération des articles:', error));
-                   })()
+                fetch(apiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        const destinationList = document.querySelector('.destination__list');
+                        data.forEach(article => {
+                            const articleElement = document.createElement('div');
+                            console.log(article.title.rendered)
+                            // <div>${article.excerpt.rendered}</div>
+                            articleElement.innerHTML = `
+                                <h3>${article.title.rendered}</h3>
+            
+                                <a href="${article.link}">Lire plus</a>
+                            `;
+                            destinationList .appendChild(articleElement);
+                        });
+                    })
+                    .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+            })()
             })
             .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+        }
 
 
 }); //()
