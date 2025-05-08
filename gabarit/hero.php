43e7e17b94a420fd5b5a091b2a87a861e41730df
mazpen <Azpensbri@gmail.com>
2025-05-08 09:41:42 -0400

    s14c2: optimization caroussel

--- gabarit/hero.php
+++ gabarit/hero.php
@@ -1,43 +1,46 @@
 <?php  
-$hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); 
-
-for ($k=0; $k<3; $k++){
-$hero_background[$k] = get_theme_mod('hero_background_' . $k, 'Default Title'); 
-}
-?>
-<section class="hero">
-    <!-- hero__carrousel -->
-    <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[0] ?>)"></div>
+    $hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+    for ($k=0; $k<3; $k++){
+    $hero_background[$k] = get_theme_mod('hero_background_'. $k, '');
+    }
+     ?>
+    <section class="hero">
+        <div class="hero__carrousel  hero__carrousel--active  " style="background-image: url(<?php echo $hero_background[0] ?>)"></div>
         <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[1] ?>)"></div>
         <div class="hero__carrousel" style="background-image: url(<?php echo $hero_background[2] ?>)"></div>
         <div class="hero__radio">
-            <input  class="hero__radio__input" data-id_radio="0" type="radio" name="carroussel"  checked="checked">
+            <input  class="hero__radio__input" data-id_radio="0"   type="radio" name="carroussel"  checked="checked">
             <input  class="hero__radio__input" data-id_radio="1" type="radio" name="carroussel">
             <input  class="hero__radio__input" data-id_radio="2" type="radio" name="carroussel">
         </div>
-        <script>
-            console.log("test inline JS OK");
-        </script>
-    <!-- hero__contenu -->
-    <div class="hero__contenu global">
-        <h1 class="hero__titre">
-            <?php  bloginfo('name'); ?>
-        </h1>
-        <p class="hero__description">
-        <?php  bloginfo('description'); ?>
-        </p>
-        <a href="" class="hero__courriel">
-            info@cmaisonneuve.qc.ca
-        </a>
-        <button class="hero__bouton">
-            Inscription
-        </button>
-        <div class="hero__icone-app">
-            <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
-            <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-        </div>
-        <p>Auteur:<?php echo $hero_auteur;  ?></p>
+        <div class="hero__contenu global">
+            <div class="hero__animation hero__animation--active">
+                <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
+                <p class="hero__description"><?php bloginfo('description'); ?></p>
+            </div>
+            <div class="hero__animation ">
+                <h1 class="hero__titre">titre animation hero1</h1>
+                <p class="hero__description">description animation hero1 </p>
+            </div>
+            <div class="hero__animation ">
+                <h1 class="hero__titre">titre animation hero2</h1>
+                <p class="hero__description">description animation hero2</p>
+            </div>
+
+   
+
+            <p class="hero__courriel">
+            <?php bloginfo('admin_email'); ?>
+            </p>
+            <p class="hero__adresse">
+                5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
+            </p>
+            <p class="hero__auteur">Auteur : <?php  echo $hero_auteur ?></p>
+            <div class="hero__icone">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
+            </div>
         </div>
     </section>
--- js/destination.js
+++ js/destination.js
@@ -1,55 +1,46 @@
 (function(){
-    console.log("vive Javascript")
-    
-        let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
-        //const domaine = "http://localhost/4w4-gr1"
-        const domaine = window.location.href
-        //const apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
-        let apiUrl =  `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
-        const categorie__ul__li = document.querySelectorAll(".categorie__ul__li")
-        console.log("categorie__ul__li.length", categorie__ul__li.length)
-        mon_fetch(apiUrl)
-        categorie__ul__li.forEach(li => {
-            li.addEventListener("mousedown",function(){
-               console.log(li.dataset.id) 
-               categoryId = li.dataset.id
-               apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${categoryId}`;
-               console.log("apiUrl = ", apiUrl)
-               mon_fetch(apiUrl)
-            })
-            
+    console.log("destination.js")
+    let categoryId = 3; // Remplacez par l'ID de la catégorie souhaitée
+    const domaine = document.querySelector('base').href;
+    /* la technique utilisée pour extraire l'url doit êtere généralisée */
+    //const domaine = window.origin + "/4w4-h2025/"
+
+    parcourir_bouton()
+    mon_fetch(categoryId)
+
+function parcourir_bouton(){
+    const categorie__ul__li = document.querySelectorAll(".categorie__ul__li")
+    console.log("categorie__ul__li.length = ", categorie__ul__li.length)
+    categorie__ul__li.forEach(elm => {
+        elm.addEventListener('mousedown', function(){
+            console.log(elm.tagName)
+            console.log("elm.dataset.category_id = " , elm.dataset.category_id)
+            mon_fetch(elm.dataset.category_id)
         })
+    })
 
-        function mon_fetch(apiUrl)
-        {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                //  <div>${article.excerpt.rendered}</div>
-                const destinationList = document.querySelector('.destination__list');
-                destinationList.innerHTML = ""
-                data.forEach(article => {
-                    const articleElement = document.createElement('div');
-                    articleElement.innerHTML = `
-                        <h3 class="destination__titre">${article.title.rendered}</h3>
-                        <div class="destination__description">
-                            <p>${article.excerpt.rendered}</p>
-                        </div>
-                        <a href="${article.link}">Lire plus</a>
-                    `;
+}
 
-                    // Ajoute l'événement pour l'accordéon
-                    const titre = articleElement.querySelector('.destination__titre');
-                    const description = articleElement.querySelector('.destination__description');
-
-                    titre.addEventListener('click', function () {
-                        description.classList.toggle('open');
-                    });
-
-                    destinationList .appendChild(articleElement);
-                });
-            })
-            .catch(error => console.error('Erreur lors de la récupération des articles:', error));
-        }
-
+function mon_fetch(id_category)
+{
+    apiUrl = `${domaine}wp-json/wp/v2/posts?categories=${id_category}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const destinationList = document.querySelector('.destination__list');
+            destinationList.innerHTML = ""
+            data.forEach(article => {
+                const articleElement = document.createElement('div');
+                console.log(article.title.rendered)
+                // <div>${article.excerpt.rendered}</div>
+                articleElement.innerHTML = `
+                    <h3>${article.title.rendered}</h3>
+                    <p>${article.excerpt.rendered}</p>
+                    <a href="${article.link}">Lire plus</a>
+                `;
+                destinationList .appendChild(articleElement);
+            });
+        })
+        .catch(error => console.error('Erreur lors de la récupération des articles:', error));
+    }  
 })()
